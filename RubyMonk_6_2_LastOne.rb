0902cf679102a620dbb71e9196b6eb6cfaaecc0d
Ricardo Manuel Costa <NB23294@novabase.pt>
2017-06-30 19:41:46 +0100

    Hp new file

---
+++ RubyMonk_6_2_LastOne.rb
@@ -0,0 +1,16 @@
+puts "The Beginning..."
+# def Calculate
+#   0
+# end
+#
+# def add(*nums)
+#   nums.inject {|sum, n| sum + n}
+# end
+#
+# def subtract
+#   1
+# end
+
+
+#puts add(-10,2,3)
+puts "The End"
